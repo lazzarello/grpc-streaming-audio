ba831239513bbfb3288fd23aa43b1d68dac0e87d
Lee Azzarello <lee@rockingtiger.com>
2025-03-11 15:46:05 -0700

    get the client and the server to connect

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -24,7 +24,7 @@ All application logic and device state management will be implemented server-sid
 These companion files are the output of:
 
 ```bash
-python -m grpc_tools.protoc -I=. --python_out=. --pyi_out=. --grpc_python_out=.
+python -m grpc_tools.protoc -I=. --python_out=. --pyi_out=. --grpc_python_out=. comms.proto
 ```
 
 ## Provided client code (`client.py`)
--- server.py
+++ server.py
@@ -1,7 +1,42 @@
+from concurrent import futures
+import logging
 import grpc
+import comms_pb2
+import comms_pb2_grpc
 
-### Your code here ###
+# implement functions outside of the proto class here. Helper functions.
+def event_helper(event):
+    pass
+
+class DeviceServiceServicer(comms_pb2_grpc.DeviceServiceServicer):
+    def StatusStream(self, request_iterator, context):
+        '''
+        Get the device status from the client, then loop over the request_iterator for status requests.
+        '''
+        return comms_pb2.DeviceStatusRequest(get=True)
+
+    def EventStream(self, request_iterator, context):
+        '''
+        When the client and server have acknowledged the event, return an ack, then loop over the request_iterator for events.
+        Write some event handler functions.
+        '''
+        return comms_pb2.DeviceEventResponse(ack=True)
+
+    def ServerAudioStream(self, request, context):
+        '''
+        Audio stream should have a logic block to look for the start message, then yield to the data message until the end message is received.
+        '''
+        return super().ServerAudioStream(request, context)
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    comms_pb2_grpc.add_DeviceServiceServicer_to_server(DeviceServiceServicer(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
 
 if __name__ == "__main__":
-    ##### Your code here #####
-    print("Hello, World!")
+    logging.basicConfig()
+    serve()
